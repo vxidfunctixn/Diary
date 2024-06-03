cae48ef8f698597297b1dcc69daf0e308d32b9ad
Marcin Kalinowski <marcin36k@outlook.com>
2024-06-04 01:22:25 +0200

    Change calendar widget to navigation widget

---
+++ src/components/widget-navigation.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Section from '@/components/section.vue'
+import Button from '@/components/button.vue'
+</script>
+
+<template>
+  <Section title="Nawigacja">
+    <div class="navigation-grid">
+      <Button icon="note-list">Lista wpisów</Button>
+      <Button icon="add-note">Dodaj notatkę</Button>
+      <Button icon="date">Zobacz miesiąc</Button>
+      <Button icon="date">Zobacz rok</Button>
+      <Button icon="search">Wyszukiwarka</Button>
+      <Button icon="settings">Ustawienia</Button>
+      <Button icon="lock">Zablokuj</Button>
+      <Button icon="info">O aplikacji</Button>
+    </div>
+  </Section>
+</template>
+
+<style lang="scss" scoped>
+.navigation-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 12px;
+  padding: 12px;
+}
+</style>
--- src/views/cockpit.vue
+++ src/views/cockpit.vue
@@ -1,26 +1,24 @@
 <script setup>
-import WidgetCalendar from '@/components/widget-calendar.vue'
+import WidgetNavigation from '@/components/widget-navigation.vue'
 import WidgetCompleteNotes from '@/components/widget-complete-notes.vue'
 </script>
 
 <template>
   <div class="cockpit">
-    <WidgetCalendar/>
+    <WidgetNavigation/>
     <WidgetCompleteNotes/>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .cockpit {
-  padding: 12px 24px;
+  padding: 24px;
   column-count: 2;
   column-gap: 24px;
 
   & > div {
     break-inside: avoid;
     margin-bottom: 24px;
-    position: relative;
-    top: 12px;
   }
 
   @media (max-width: 700px) {
